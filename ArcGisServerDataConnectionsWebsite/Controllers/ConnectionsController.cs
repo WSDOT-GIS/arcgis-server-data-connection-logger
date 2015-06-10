4be3fe29e929729fcbe0a7770b6977b317475e9c
JeffJacobson <JeffJacobson@users.noreply.github.com>
2015-06-09 17:27:23 -0700

    Modified controller to group by directory

--- ArcGisServerDataConnectionsWebsite/Controllers/ConnectionsController.cs
+++ ArcGisServerDataConnectionsWebsite/Controllers/ConnectionsController.cs
@@ -14,14 +14,19 @@ namespace ArcGisServerDataConnectionsWebsite.Controllers
     public class ConnectionsController : ApiController
     {
         [Route("api/connections")]
-        public IEnumerable<IEnumerable<DataConnectionInfo>> GetConnections()
+        public Dictionary<string,Dictionary<string,IEnumerable<DataContracts.DataConnectionInfo>>> GetConnections()
         {
             var dirListString = ConfigurationManager.AppSettings["directories"];
             var dirs = from s in dirListString.Split(';') 
                        select new DirectoryInfo(s);
 
-            var datasetInfos = dirs.GetDatasetInfos();
-            return datasetInfos.SelectMany(s => s);
+            ////var datasetInfos = dirs.GetDatasetInfos();
+            ////return datasetInfos.SelectMany(s => s);
+
+            var output = (from d in dirs
+                         select new { key = d.FullName, value = d.GetDatasetInfos().Where(k => k.Count() > 0).ToDictionary(k => k.First().Msd, v => v) }).ToDictionary(k => k.key, v => v.value);
+
+            return output;
 
             //ArcGisConnection.DatasetInfoCollector.GetDatasetInfos()
         }
