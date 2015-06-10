c877c8337988be0da3d92a97455af37c7c066a3b
JeffJacobson <JeffJacobson@users.noreply.github.com>
2015-06-09 17:12:46 -0700

    Added WebAPI controller.

---
+++ ArcGisServerDataConnectionsWebsite/Controllers/ConnectionsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ArcGisConnection;
+using DataContracts;
+
+namespace ArcGisServerDataConnectionsWebsite.Controllers
+{
+    public class ConnectionsController : ApiController
+    {
+        [Route("api/connections")]
+        public IEnumerable<IEnumerable<DataConnectionInfo>> GetConnections()
+        {
+            var dirListString = ConfigurationManager.AppSettings["directories"];
+            var dirs = from s in dirListString.Split(';') 
+                       select new DirectoryInfo(s);
+
+            var datasetInfos = dirs.GetDatasetInfos();
+            return datasetInfos.SelectMany(s => s);
+
+            //ArcGisConnection.DatasetInfoCollector.GetDatasetInfos()
+        }
+    }
+}
